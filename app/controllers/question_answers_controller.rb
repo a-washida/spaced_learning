bc1fb8833b930a8a17707fe95cd1425c82055156
a-washida <59239304+a-washida@users.noreply.github.com>
2020-08-18 16:55:40 +0900

    問題作成機能を実装

--- app/controllers/question_answers_controller.rb
+++ app/controllers/question_answers_controller.rb
@@ -1,11 +1,27 @@
 class QuestionAnswersController < ApplicationController
+  before_action :set_group, only: [:new, :create]
+  before_action :question_params, only: :create
 
   def new
-    @group = current_user.groups.find(params[:group_id])
     @question_answer = QuestionAnswer.new
   end
 
   def create
+    @question_answer = QuestionAnswer.new(question_params)
+    if @question_answer.save
+      redirect_to new_group_question_answer_path(@group)
+    else
+      render 'new'
+    end
   end
 
+  private
+
+  def question_params
+    params.require(:question_answer).permit(:question, :answer).merge(display_date: Date.today.yday, memory_level: 0, repeat_count: 0, user_id: current_user.id, group_id: params[:group_id])
+  end
+
+  def set_group
+    @group = current_user.groups.find(params[:group_id])
+  end
 end
