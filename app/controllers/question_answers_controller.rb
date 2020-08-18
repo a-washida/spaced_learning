2fccaa4d51e8cdb1b67f84e9b23f1835eeaa0536
a-washida <59239304+a-washida@users.noreply.github.com>
2020-08-18 16:38:41 +0900

    問題作成ページにフォームを作成

--- app/controllers/question_answers_controller.rb
+++ app/controllers/question_answers_controller.rb
@@ -1,6 +1,8 @@
 class QuestionAnswersController < ApplicationController
-  
+
   def new
+    @group = current_user.groups.find(params[:group_id])
+    @question_answer = QuestionAnswer.new
   end
 
   def create
