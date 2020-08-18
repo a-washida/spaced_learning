f6a9b3fae8a11006de32601fe74106da9d8dd908
a-washida <59239304+a-washida@users.noreply.github.com>
2020-08-18 17:02:18 +0900

    バリデーションを記述

--- app/models/question_answer.rb
+++ app/models/question_answer.rb
@@ -1,2 +1,13 @@
 class QuestionAnswer < ApplicationRecord
+  with_options presence: true do
+    validates :question
+    validates :answer
+
+    with_options numericality: true do
+      validates :display_date
+      validates :memory_level
+      validates :repeat_count
+    end
+  end
+
 end
