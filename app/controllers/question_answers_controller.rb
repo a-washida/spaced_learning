f88d67c2ad079d07f3f060964d84f934782fed01
a-washida <59239304+a-washida@users.noreply.github.com>
2020-08-18 15:03:14 +0900

    問題作成のパネルをクリックすると問題作成ページに遷移するように設定

--- app/controllers/question_answers_controller.rb
+++ app/controllers/question_answers_controller.rb
@@ -1,2 +1,9 @@
 class QuestionAnswersController < ApplicationController
+  
+  def new
+  end
+
+  def create
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root 'groups#index'
   resources :users, only: [:edit, :update]
-  resources :groups, only: [:index, :create, :update]
+  resources :groups, only: [:index, :create, :update] do
+    resources :question_answers, only: [:new, :create]
+  end
 end
