486bd6494fce5440328cbfe16d46b441c18ac911
a-washida <59239304+a-washida@users.noreply.github.com>
2020-08-19 19:34:35 +0900

    question_optionとanswer_optionのモデルとテーブルを作成

---
+++ db/migrate/20200819102831_create_question_options.rb
@@ -0,0 +1,10 @@
+class CreateQuestionOptions < ActiveRecord::Migration[6.0]
+  def change
+    create_table :question_options do |t|
+      t.references :question_answer, foreign_key: true
+      t.integer :font_size_id,       null: false
+      t.integer :image_size_id,      null: false
+      t.timestamps
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_18_080908) do
+ActiveRecord::Schema.define(version: 2020_08_19_103253) do
 
   create_table "active_storage_attachments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
@@ -31,6 +31,15 @@ ActiveRecord::Schema.define(version: 2020_08_18_080908) do
     t.string "checksum", null: false
     t.datetime "created_at", null: false
     t.index ["key"], name: "index_active_storage_blobs_on_key", unique: true
+  end
+
+  create_table "answer_options", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.bigint "question_answer_id"
+    t.integer "font_size_id", null: false
+    t.integer "image_size_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["question_answer_id"], name: "index_answer_options_on_question_answer_id"
   end
 
   create_table "groups", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
@@ -55,6 +64,15 @@ ActiveRecord::Schema.define(version: 2020_08_18_080908) do
     t.index ["user_id"], name: "index_question_answers_on_user_id"
   end
 
+  create_table "question_options", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.bigint "question_answer_id"
+    t.integer "font_size_id", null: false
+    t.integer "image_size_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["question_answer_id"], name: "index_question_options_on_question_answer_id"
+  end
+
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "nickname", null: false
     t.string "email", default: "", null: false
@@ -69,7 +87,9 @@ ActiveRecord::Schema.define(version: 2020_08_18_080908) do
   end
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
+  add_foreign_key "answer_options", "question_answers"
   add_foreign_key "groups", "users"
   add_foreign_key "question_answers", "groups"
   add_foreign_key "question_answers", "users"
+  add_foreign_key "question_options", "question_answers"
 end
