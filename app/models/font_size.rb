6acd473c490c642dfa3876455fb6279a9c942fcf
a-washida <59239304+a-washida@users.noreply.github.com>
2020-08-20 12:07:47 +0900

    ActiveHashのモデルを仮作成

---
+++ app/models/font_size.rb
@@ -0,0 +1,7 @@
+class FontSize < ActiveHash::Base
+  self.data = [
+    { id: 1, name: '10' },
+    { id: 2, name: '15' },
+    { id: 3, name: '20' }
+  ]
+end
---
+++ app/models/image_size.rb
@@ -0,0 +1,7 @@
+class ImageSize < ActiveHash::Base
+  self.data = [
+    { id: 1, name: '100%' },
+    { id: 2, name: '80%' },
+    { id: 3, name: '60%' }
+  ]
+end
