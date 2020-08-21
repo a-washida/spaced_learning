b51ac9fa44bb02d58e071dc66f1357a8cdb88dbb
a-washida <59239304+a-washida@users.noreply.github.com>
2020-08-21 11:49:57 +0900

    pulldown_font_image.jsにコメントを追加

--- app/javascript/pulldown_font_image.js
+++ app/javascript/pulldown_font_image.js
@@ -1,33 +1,42 @@
+// DRY気になる
+
+// プルダウンの項目を選択すると、プレビューエリアのフォントサイズや画像サイズを変化させる関数
 function pulldownFontOrImageSize(){
   
+  // プルダウンの項目を選択すると、プレビューエリアのフォントサイズを変化させる関数式
   const pulldownFontFunction = (questionOrAnswer) => {
     const pulldownElement = document.getElementById(`question_answer_with_option_${questionOrAnswer}_font_size_id`)
+    // プルダウンの項目を選択することでイベント発火する
     pulldownElement.addEventListener("change", (e) => {
+      // 現在選択されているブルダウンの項目が、上から何番目の項目なのかを取得
       const value = pulldownElement.value
       const pulldownOptions = document.querySelectorAll(`#question_answer_with_option_${questionOrAnswer}_font_size_id option`);
+      // 現在選択されているプルダウンの項目の要素(<option>)を取得
       const chosenDropdownList = pulldownOptions[value - 1];
+      // フォントサイズのデフォルト(100%)に、プルダウンの選択した項目の値をかける
+      const fontSize = 100 * chosenDropdownList.innerHTML
       const previewTextarea = document.getElementById(`insert-preview-${questionOrAnswer}-textarea`)
-      previewTextarea.setAttribute("style", `font-size: ${chosenDropdownList.innerHTML}px;`)
+      previewTextarea.setAttribute("style", `font-size: ${fontSize}%;`)
     })
   }
   pulldownFontFunction("question")
   pulldownFontFunction("answer")
 
+  // プルダウンの項目を選択すると、プレビューエリアの画像サイズを変化させる関数式
   const pulldownImageFunction = (questionOrAnswer) => {
     const pulldownElement = document.getElementById(`question_answer_with_option_${questionOrAnswer}_image_size_id`)
     pulldownElement.addEventListener("change", (e) => {
       const value = pulldownElement.value
       const pulldownOptions = document.querySelectorAll(`#question_answer_with_option_${questionOrAnswer}_image_size_id option`);
       const chosenDropdownList = pulldownOptions[value - 1];
-      const previewImage = document.querySelector(`.img-preview-${questionOrAnswer}`)
+      // 画像の横幅のデフォルト(200px)に、プルダウンの選択した項目の値をかける
       const imageWidth = 200 * chosenDropdownList.innerHTML
+      const previewImage = document.querySelector(`.img-preview-${questionOrAnswer}`)
       previewImage.setAttribute("style", `width: ${imageWidth}px;`)
     })
   }
   pulldownImageFunction("question")
   pulldownImageFunction("answer")
-
-  
 }
 
 window.addEventListener("load", pulldownFontOrImageSize);
