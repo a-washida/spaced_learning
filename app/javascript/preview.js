c9beb50312533ef8d2876181668ba4474e8e7a4a
a-washida <59239304+a-washida@users.noreply.github.com>
2020-08-21 11:07:04 +0900

    すでに画像がある場合に再度画像を取り込もうとした場合、画像サイズを引き継ぐように変更

--- app/javascript/preview.js
+++ app/javascript/preview.js
@@ -1,3 +1,6 @@
+// 画像がある状態で、画像ボタンをクリックした後にキャンセルするとエラーが出るのなんとかしたい
+
+// 問題欄もしくは解答欄で画像を取り込んだ場合に、その画像をプレビュー表示する関数
 function imagePreview(){
   const hiddenFileFields = document.querySelectorAll('.hidden')
   const arrays = ["question", "answer"] 
@@ -5,17 +8,27 @@ function imagePreview(){
     hiddenFileFields[i].addEventListener('change', (e) => {
       // 画像を取り込むと、その画像をプレビュー表示する関数式
       const questionOrAnswerImagePreview = (questionOrAnswer) => {
-        // 画像が表示されている場合のみ、すでに存在している画像を削除する
         const imageContent = document.querySelector(`.img-preview-${questionOrAnswer}`);
+        let imageElement = ""
         if (imageContent){
+          if(imageContent.hasAttribute("style")){
+            // もし画像が存在し、さらにstyle属性を持つならば、styleの属性値を変数imageElementに代入
+            imageElement = imageContent.getAttribute("style")
+          }
+          // 画像がブラウザ上に存在する場合のみ、すでに存在している画像を削除する
           imageContent.remove();
         };
+
         // 以下5行で、取り込んだ画像を表示する<img>要素作成
         const file = e.target.files[0];
         const blob = window.URL.createObjectURL(file);
         const blobImage = document.createElement('img')
         blobImage.setAttribute('src', blob)
         blobImage.setAttribute("class", `img-preview-${questionOrAnswer}`)
+        // もしimageElementの値が存在するなら、上記で作成した<img>要素にstyle属性とその属性値としてimageElementを追加
+        if (imageElement){
+          blobImage.setAttribute("style", `${imageElement}`)
+        }
 
         const previewImage = document.getElementById(`insert-preview-image-${questionOrAnswer}`)
         previewImage.appendChild(blobImage)
