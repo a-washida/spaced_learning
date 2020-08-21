2350b09124ec9410b16ed4099c8876d53e7a64b1
a-washida <59239304+a-washida@users.noreply.github.com>
2020-08-21 11:24:10 +0900

    activeHashに登録するデータを変更

--- app/models/font_size.rb
+++ app/models/font_size.rb
@@ -1,7 +1,14 @@
 class FontSize < ActiveHash::Base
   self.data = [
-    { id: 1, name: '10' },
-    { id: 2, name: '15' },
-    { id: 3, name: '20' }
+    { id: 1, name: '4' },
+    { id: 2, name: '3' },
+    { id: 3, name: '2' },
+    { id: 4, name: '1.5' },
+    { id: 5, name: '1.2' },
+    { id: 6, name: '1' },
+    { id: 7, name: '0.9' },
+    { id: 8, name: '0.8' },
+    { id: 9, name: '0.7' },
+    { id: 10, name: '0.6' }
   ]
 end
