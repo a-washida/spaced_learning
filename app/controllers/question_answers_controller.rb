eb4c42d7dd57c2b3294bae8c4f11d85ae3158668
a-washida <59239304+a-washida@users.noreply.github.com>
2020-08-23 16:18:11 +0900

    editアクションをupdateアクションの内容を記述

--- app/controllers/question_answers_controller.rb
+++ app/controllers/question_answers_controller.rb
@@ -1,5 +1,6 @@
 class QuestionAnswersController < ApplicationController
-  before_action :set_group, only: [:index, :show]
+  before_action :set_group, only: [:index, :show, :edit, :update]
+  before_action :set_question_answer, only: [:edit, :update]
   before_action :question_answer_with_option_params, only: :create
 
   def index
@@ -29,6 +30,11 @@ class QuestionAnswersController < ApplicationController
   end
 
   def update
+    if @question_answer.update(nest_params)
+      redirect_to group_question_answer_path(@group, @question_answer)
+    else
+      render 'edit'
+    end
   end
 
   private
@@ -37,7 +43,18 @@ class QuestionAnswersController < ApplicationController
     @group = Group.find(params[:group_id])
   end
 
+  def set_question_answer
+    @question_answer = QuestionAnswer.find(params[:id])
+  end
+
   def question_answer_with_option_params
     params.require(:question_answer_with_option).permit(:question, :question_image, :answer, :answer_image, :question_font_size_id, :question_image_size_id, :answer_font_size_id, :answer_image_size_id).merge(user_id: current_user.id, group_id: params[:group_id])
+  end
+
+  def nest_params
+    params.require(:question_answer).permit(:question, :answer, 
+                                            question_option_attributes: [:image, :font_size_id, :image_size_id, :id], 
+                                            answer_option_attributes: [:image, :font_size_id, :image_size_id, :id]
+                                            )
   end
 end
