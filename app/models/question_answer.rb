68b29ee4aacdc91087796fad0fa75b7e4f7c810b
a-washida <59239304+a-washida@users.noreply.github.com>
2020-08-23 19:17:56 +0900

    テキストもしくは画像が必須の制限をかけるバリデーションを追加

--- app/models/question_answer.rb
+++ app/models/question_answer.rb
@@ -1,4 +1,6 @@
 class QuestionAnswer < ApplicationRecord
+  validate :question_text_or_image_indispensable
+  validate :answer_text_or_image_indispensable
   with_options presence: true do
     with_options numericality: true do
       validates :display_date
@@ -6,7 +8,7 @@ class QuestionAnswer < ApplicationRecord
       validates :repeat_count
     end
   end
-
+  
   belongs_to :user
   belongs_to :group
   has_one :question_option
@@ -14,4 +16,16 @@ class QuestionAnswer < ApplicationRecord
 
   accepts_nested_attributes_for :question_option
   accepts_nested_attributes_for :answer_option
+
+  private
+
+  def question_text_or_image_indispensable
+    return if question.present? || question_option.image.present?
+    errors.add(:question, 'text or image is indispensable')
+  end
+
+  def answer_text_or_image_indispensable
+    return if answer.present? || answer_option.image.present?
+    errors.add(:answer, 'text or image is indispensable')
+  end
 end
