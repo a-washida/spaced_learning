45a500edb176d5ec44be9d28a46eff39583e9cc1
a-washida <59239304+a-washida@users.noreply.github.com>
2020-08-24 11:21:32 +0900

    question_answerのレコードを削除すると、question_optionとanswer_optionもまとめて削除

--- app/models/question_answer.rb
+++ app/models/question_answer.rb
@@ -11,8 +11,8 @@ class QuestionAnswer < ApplicationRecord
 
   belongs_to :user
   belongs_to :group
-  has_one :question_option
-  has_one :answer_option
+  has_one :question_option, dependent: :destroy
+  has_one :answer_option, dependent: :destroy
 
   accepts_nested_attributes_for :question_option
   accepts_nested_attributes_for :answer_option
