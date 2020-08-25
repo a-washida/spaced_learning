bda631b604f79bd05e750dc9a93b1472a70a183d
a-washida <59239304+a-washida@users.noreply.github.com>
2020-08-25 17:57:30 +0900

    コードのリファクタリングと、保存の際にトランザクションを適用

--- app/controllers/repetition_algorithms_controller.rb
+++ app/controllers/repetition_algorithms_controller.rb
@@ -4,11 +4,8 @@ class RepetitionAlgorithmsController < ApplicationController
   def create
     question_answer = QuestionAnswer.find(params[:question_answer_id])
     repetition_algorithm = question_answer.repetition_algorithm
-
     repetition_service = RepetitionAlgorithmService.new(review_params, question_answer, repetition_algorithm)
-    repetition_service.execute_repetition_algorithm_considering_conditional_branch(params[:repeat_count], params[:memory_level])
-    post = "次は#{repetition_algorithm.interval}日後です"
-
+    post = repetition_service.execute_repetition_algorithm_considering_conditional_branch(params[:repeat_count], params[:memory_level])
     render json: { post: post }
   end
 
--- app/services/repetition_algorithm_service.rb
+++ app/services/repetition_algorithm_service.rb
@@ -35,28 +35,36 @@ class RepetitionAlgorithmService
 
   private
 
-  def repetition_algorithm_if_repeat_count_equal_0(next_interval, difference_of_ef)
+  def repetition_algorithm_if_repeat_count_equal_0(next_interval, difference_of_easiness_factor)
     @review_params[:interval] = next_interval
-    @review_params[:easiness_factor] = @review_params[:easiness_factor].to_i + difference_of_ef
-    next_display_date = Date.today.yday + @review_params[:interval]
-    @repetition_algorithm.update(interval: @review_params[:interval], easiness_factor: @review_params[:easiness_factor] )
-    @question_answer.update(display_date: next_display_date, memory_level: @review_params[:memory_level], repeat_count: @review_params[:repeat_count].to_i + 1)
+    common_part_of_repetition_algorithm(difference_of_easiness_factor)
   end
 
-  def repetition_algorithm_if_repeat_count_greater_than_0(memory_level, difference_of_ef, num)
-    modified_easiness_factor = @review_params[:easiness_factor].to_i - 80 + 28 * memory_level - 2 * memory_level * memory_level
+  def repetition_algorithm_if_repeat_count_greater_than_0(memory_level, difference_of_easiness_factor, num)
+    modified_easiness_factor = @review_params[:easiness_factor].to_i - 80 + 28 * memory_level - 2 * memory_level ** 2
     @review_params[:interval] = @review_params[:interval].to_f * modified_easiness_factor/100 + num
+    limit_interval_range_1_to_100()
+    common_part_of_repetition_algorithm(difference_of_easiness_factor)
+  end
+
+  def common_part_of_repetition_algorithm(difference_of_easiness_factor)
+    @review_params[:easiness_factor] = @review_params[:easiness_factor].to_i + difference_of_easiness_factor
+    next_display_date = Date.today.yday + @review_params[:interval].to_i
+    ActiveRecord::Base.transaction do
+      @repetition_algorithm.update!(interval: @review_params[:interval].round(4), easiness_factor: @review_params[:easiness_factor] )
+      @question_answer.update!(display_date: next_display_date, memory_level: @review_params[:memory_level], repeat_count: @review_params[:repeat_count].to_i + 1)
+    end
+    return "次は#{@repetition_algorithm.interval}日後です"
+    rescue => e
+      return "エラーが発生しました"
+  end
+
+  def limit_interval_range_1_to_100()
     if @review_params[:interval].to_f > 100
       @review_params[:interval] = 100
     end
     if @review_params[:interval].to_f < 1
       @review_params[:interval] = 1
     end
-    @review_params[:easiness_factor] = @review_params[:easiness_factor].to_i + difference_of_ef
-    next_display_date = Date.today.yday + @review_params[:interval].to_i
-
-    @repetition_algorithm.update(interval: @review_params[:interval].round(4), easiness_factor: @review_params[:easiness_factor])
-    @question_answer.update(display_date: next_display_date, memory_level: @review_params[:memory_level], repeat_count: @review_params[:repeat_count].to_i + 1)
   end
-
 end
