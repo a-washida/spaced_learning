10040c3fc64eef04b9364a909d8401ab4e754bf4
a-washida <59239304+a-washida@users.noreply.github.com>
2020-08-26 19:29:03 +0900

    updateの際に、更新してはいけないカラムのキーをexceptで除外

--- app/controllers/question_answers_controller.rb
+++ app/controllers/question_answers_controller.rb
@@ -1,5 +1,5 @@
 class QuestionAnswersController < ApplicationController
-  before_action :set_group, only: [:index, :show, :new, :create, :edit, :update, :destroy, :review]
+  before_action :set_group
   before_action :set_question_answer, only: [:show, :edit, :update, :destroy]
 
   def index
@@ -16,7 +16,7 @@ class QuestionAnswersController < ApplicationController
   end
 
   def create
-    @question_answer = QuestionAnswer.new(nest_params)
+    @question_answer = QuestionAnswer.new(nested_form_params)
     ActiveRecord::Base.transaction do
       @question_answer.save!
       RepetitionAlgorithm.create!(interval: 0, easiness_factor: 200, question_answer_id: @question_answer.id)
@@ -30,8 +30,7 @@ class QuestionAnswersController < ApplicationController
   end
 
   def update
-    # nest_paramsから.merge以降を削除する必要あり
-    if @question_answer.update(nest_params)
+    if @question_answer.update(nested_form_params.except(:display_date, :display_year, :memory_level, :repeat_count))
       redirect_to group_question_answer_path(@group, @question_answer)
     else
       render 'edit'
@@ -63,7 +62,7 @@ class QuestionAnswersController < ApplicationController
     @question_answer = QuestionAnswer.find(params[:id])
   end
 
-  def nest_params
+  def nested_form_params
     params.require(:question_answer).permit(:question, :answer,
                                             question_option_attributes: [:image, :font_size_id, :image_size_id, :id],
                                             answer_option_attributes: [:image, :font_size_id, :image_size_id, :id]).
