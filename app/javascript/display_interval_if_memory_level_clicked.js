cc81c5beb6ca09f674dc6f946e92facecc249f9e
a-washida <59239304+a-washida@users.noreply.github.com>
2020-08-27 12:42:37 +0900

    記憶度ボタンを押すとインターバルを表示するjsファイルをリファクタリング

--- app/javascript/display_interval_if_memory_level_clicked.js
+++ app/javascript/display_interval_if_memory_level_clicked.js
@@ -1,9 +1,11 @@
 if (window.location.pathname.includes("question_answers/review")){
-  function repetition_algorithm() {
+  // 記憶度の評価ボタンをクリックすると、非同期通信が行われ、サーバー内で計算されたインターバルを表示する関数
+  function display_interval_if_memory_level_clicked() {
     const noticeDisplayDates = document.querySelectorAll(".review-option__notice-display-date")
     for(let i = 0; i < 4; i++) {
       const clicks = document.querySelectorAll(`.review-option__btn.js-${i}`);
       for(let j = 0; j < clicks.length; j++ ){
+        // 以下4行はHTMLタグに付与したカスタムデータ属性の値を取得
         const interval = clicks[j].getAttribute("data-interval");
         const easinessFactor = clicks[j].getAttribute("data-ef");
         const questionAnswerId = clicks[j].getAttribute("data-qa-id");
@@ -14,6 +16,7 @@ if (window.location.pathname.includes("question_answers/review")){
           XHR.open("POST", `/repetition_algorithms`, true);
           XHR.responseType = "json";
           XHR.setRequestHeader( 'content-type', 'application/x-www-form-urlencoded;charset=UTF-8' );
+          // 5つのデータをparamsに格納してサーバーへ送る
           XHR.send(`interval=${interval}&easiness_factor=${easinessFactor}&question_answer_id=${questionAnswerId}&repeat_count=${repeatCount}&memory_level=${memoryLevel}`);
           XHR.onload = () => {
             const item = XHR.response.post;
@@ -33,5 +36,5 @@ if (window.location.pathname.includes("question_answers/review")){
     };
   };
 
-  window.addEventListener("load", repetition_algorithm);
+  window.addEventListener("load", display_interval_if_memory_level_clicked);
 }
