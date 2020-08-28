bd99f1ee61c8e6799fb245847ede195923effdc3
a-washida <59239304+a-washida@users.noreply.github.com>
2020-08-28 09:33:10 +0900

    グループ削除機能を実装

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root 'groups#index'
   resources :users, only: [:edit, :update]
-  resources :groups, only: [:index, :create, :update] do
+  resources :groups, only: [:index, :create, :update, :destroy] do
     resources :question_answers do
       collection do
         get 'review'
