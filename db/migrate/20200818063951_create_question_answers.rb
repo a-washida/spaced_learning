158bb15c91b40d6d06675a91aa827220fdd0bbb3
a-washida <59239304+a-washida@users.noreply.github.com>
2020-08-30 16:31:50 +0900

    DBを一度壊した後、display_dateとdisplay_yearを統合してrails db:migrate

--- db/migrate/20200818063951_create_question_answers.rb
+++ db/migrate/20200818063951_create_question_answers.rb
@@ -1,14 +1,13 @@
 class CreateQuestionAnswers < ActiveRecord::Migration[6.0]
   def change
     create_table :question_answers do |t|
-      t.text :question
-      t.text :answer
-      t.integer :display_date, null: false
-      t.integer :display_year, null: false
-      t.integer :memory_level, null: false
-      t.integer :repeat_count, null: false
-      t.references :user, foreign_key: true
-      t.references :group, foreign_key: true
+      t.text       :question
+      t.text       :answer
+      t.date       :display_date, null: false
+      t.integer    :memory_level, null: false
+      t.integer    :repeat_count, null: false
+      t.references :user,         foreign_key: true
+      t.references :group,        foreign_key: true
       t.timestamps
     end
   end
