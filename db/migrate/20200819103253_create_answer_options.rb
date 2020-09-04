a1ce09aac80c5d233f94e79d3ccc076ea16d8fc9
a-washida <59239304+a-washida@users.noreply.github.com>
2020-09-04 23:47:32 +0900

    [修正]DBを作り直し、その際にgroupsから一意性制約を削除、その他微調整
    画像の保存先をS3に変更したため、rake db:reset db:migrateでデータベースをリセット
    その他微調整の内容は、マイグレーションファイルの可読性を改善するために、スペースを入れて見た目調整

--- db/migrate/20200819103253_create_answer_options.rb
+++ db/migrate/20200819103253_create_answer_options.rb
@@ -2,8 +2,8 @@ class CreateAnswerOptions < ActiveRecord::Migration[6.0]
   def change
     create_table :answer_options do |t|
       t.references :question_answer, foreign_key: true
-      t.integer :font_size_id,       null: false
-      t.integer :image_size_id,      null: false
+      t.integer    :font_size_id,    null: false
+      t.integer    :image_size_id,   null: false
       t.timestamps
     end
   end
--- db/migrate/20200824070709_create_repetition_algorithms.rb
+++ db/migrate/20200824070709_create_repetition_algorithms.rb
@@ -1,8 +1,8 @@
 class CreateRepetitionAlgorithms < ActiveRecord::Migration[6.0]
   def change
     create_table :repetition_algorithms do |t|
-      t.float :interval, null: false
-      t.integer :easiness_factor, null: false
+      t.float      :interval,        null: false
+      t.integer    :easiness_factor, null: false
       t.references :question_answer, foreign_key: true
       t.timestamps
     end
