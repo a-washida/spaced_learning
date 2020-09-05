ea3c424fa55091252887d2a396b31aaf0a77eda5
a-washida <59239304+a-washida@users.noreply.github.com>
2020-09-05 20:42:20 +0900

    rubocopを実行

--- app/helpers/question_answers_helper.rb
+++ app/helpers/question_answers_helper.rb
@@ -2,11 +2,11 @@ module QuestionAnswersHelper
   # 復習までの日数を、dateに応じて条件分岐して値を返すメソッド
   def display_date_until_review(date)
     if date >= 0 && date <= 100
-      "#{date}"
+      date.to_s
     elsif date < 0
-      "0"
+      '0'
     else
-      "--"
+      '--'
     end
   end
 end
