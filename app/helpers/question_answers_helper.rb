3dbcdcfb6992781bada23222657df99b91c25851
a-washida <59239304+a-washida@users.noreply.github.com>
2020-09-06 15:04:46 +0900

    [追加]プルダウンのselectedの値を返すヘルパーメソッドを作成

--- app/helpers/question_answers_helper.rb
+++ app/helpers/question_answers_helper.rb
@@ -1,4 +1,22 @@
 module QuestionAnswersHelper
+  # font_size_idが存在すればfont_size_idを、存在しなければ6を返すメソッド
+  def set_font_size_id(font_size_id)
+    if font_size_id.present?
+      font_size_id
+    else
+      6
+    end
+  end
+
+  # image_size_idが存在すればimage_size_idを、存在しなければ4を返すメソッド
+  def set_image_size_id(image_size_id)
+    if image_size_id.present?
+      image_size_id
+    else
+      4
+    end
+  end
+
   # 復習までの日数を、dateに応じて条件分岐して値を返すメソッド
   def display_date_until_review(date)
     if date >= 0 && date <= 100
