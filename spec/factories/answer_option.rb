48971b2facd76ddfbdfcd5deb6436c264f3bcbcb
a-washida <59239304+a-washida@users.noreply.github.com>
2020-09-17 22:19:34 +0900

    [修正]factorybotの記述を修正

--- spec/factories/answer_option.rb
+++ spec/factories/answer_option.rb
@@ -3,8 +3,11 @@ FactoryBot.define do
     font_size_id { 6 }
     image_size_id { 4 }
 
-    after(:build) do |item|
-      item.image.attach(io: File.open('public/images/test_image.png'), filename: 'test_image.png')
+    # 画像を添付する場合のテストデータ
+    trait :with_image do
+      after(:build) do |item|
+        item.image.attach(io: File.open('public/images/test_image.png'), filename: 'test_image.png')
+      end
     end
   end
 end
