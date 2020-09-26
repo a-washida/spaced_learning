aedd53446dc7a1926d42377d8d67056d8d6b954c
a-washida <59239304+a-washida@users.noreply.github.com>
2020-09-26 22:12:30 +0900

    [修正]画像を取り込んだ時点でプルダウンの値を反映させるように修正
    プルダウンで選択する値を変えてから画像を取り込む場合、プルダウンの値が反映されないため

--- app/javascript/preview_if_text_or_image_input.js
+++ app/javascript/preview_if_text_or_image_input.js
@@ -6,6 +6,9 @@ window.addEventListener("load", (e) => {
                                             action === "create" ||
                                             action ===  "edit" ||
                                             action === "update")){
+    
+    // モジュールを読み込むことで、プルダウンでselectedされているoption要素を返す関数式を、変数に代入
+    const getSelectedOptionInPulldown = require('./module_get_selected_option.js')
 
     imagePreview()
     textareaPreview()
@@ -18,15 +21,10 @@ window.addEventListener("load", (e) => {
         hiddenFileFields[i].addEventListener('change', (e) => {
           // 画像を取り込むと、その画像をプレビュー表示する関数式
           const questionOrAnswerImagePreview = (questionOrAnswer) => {
-            const imageContent = document.querySelector(`.img-preview-${questionOrAnswer}`);
-            let imageElement = ""
-            if (imageContent){
-              if(imageContent.hasAttribute("style")){
-                // もし画像が存在し、さらにstyle属性を持つならば、styleの属性値を変数imageElementに代入
-                imageElement = imageContent.getAttribute("style")
-              }
-              // 画像がブラウザ上に存在する場合のみ、すでに存在している画像を削除する
-              imageContent.remove();
+            const previewImage = document.getElementById(`${questionOrAnswer}-preview__img`);
+            // previewImageに子要素が存在する場合、全て削除する(画像と削除ボタンの2つが存在する場合がある)
+            while (previewImage.firstChild){
+              previewImage.removeChild(previewImage.firstChild);
             };
 
             // 以下5行で、取り込んだ画像を表示する<img>要素作成
@@ -35,12 +33,10 @@ window.addEventListener("load", (e) => {
             const blobImage = document.createElement('img')
             blobImage.setAttribute('src', blob)
             blobImage.setAttribute("class", `img-preview-${questionOrAnswer}`)
-            // もしimageElementの値が存在するなら、上記で作成した<img>要素にstyle属性とその属性値としてimageElementを追加
-            if (imageElement){
-              blobImage.setAttribute("style", `${imageElement}`)
-            }
+            // img要素に、プルダウンで選択されている値を考慮したwidthを設定
+            const selectedOption = getSelectedOptionInPulldown(questionOrAnswer, "image")
+            blobImage.setAttribute("style", `width: ${Math.floor(200 * selectedOption.innerHTML)}px;`)
 
-            const previewImage = document.getElementById(`${questionOrAnswer}-preview__img`)
             previewImage.appendChild(blobImage)
           }
 
