4d7229646bb0303b40bc76a554486113e681ff25
a-washida <59239304+a-washida@users.noreply.github.com>
2020-10-03 23:35:17 +0900

    [追加]復習タイミングの設定の編集機能を実装

--- app/controllers/options_controller.rb
+++ app/controllers/options_controller.rb
@@ -5,11 +5,20 @@ class OptionsController < ApplicationController
   end
 
   def update
+    if @option.update(option_params)
+      redirect_to root_path, notice: '設定を変更しました'
+    else
+      render 'edit'
+    end
   end
 
   private
   
   def set_option
     @option = Option.find(params[:id])
+  end
+
+  def option_params
+    params.require(:option).permit(:interval_of_ml1, :interval_of_ml2, :interval_of_ml3, :interval_of_ml4, :easiness_factor)
   end
 end
