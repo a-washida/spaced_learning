bb5b59f4f828fb1b9051ca85619ac1747e6dba5e
a-washida <59239304+a-washida@users.noreply.github.com>
2020-10-03 23:55:33 +0900

    [追加]optionモデルにバリデーションを設定

--- app/models/option.rb
+++ app/models/option.rb
@@ -1,3 +1,13 @@
 class Option < ApplicationRecord
+  with_options presence: true do
+    with_options numericality: { greater_than: 0 } do
+      validates :interval_of_ml1
+      validates :interval_of_ml2
+      validates :interval_of_ml3
+      validates :interval_of_ml4
+    end
+    validates :easiness_factor, numericality: { greater_than_or_equal_to: 130, less_than_or_equal_to: 250 }
+  end
+
   belongs_to :user
 end
