9843f73af5536113e4626de1bbf960d73b5052db
a-washida <59239304+a-washida@users.noreply.github.com>
2020-10-20 15:39:08 +0900

    [修正]既にカテゴリー2が選択されている場合、カテゴリー1を選択した際にリンクを出現

--- app/javascript/select_category.js
+++ app/javascript/select_category.js
@@ -9,10 +9,10 @@ window.addEventListener("load", (e) => {
     const links = document.querySelectorAll(".cs-index__link")
     categoryFirst.addEventListener("change", (e) => {
       // style属性を削除する関数式
-      const removeStyle = (styles) => {
-        styles.forEach( function(style){
-          if(style.hasAttribute("style")){
-            style.removeAttribute("style")
+      const removeStyle = (elements) => {
+        elements.forEach( function(element){
+          if(element.hasAttribute("style")){
+            element.removeAttribute("style")
           }
         })
       }
@@ -27,22 +27,29 @@ window.addEventListener("load", (e) => {
       // カテゴリー1のプルダウンで「選択してください」以外が選択された場合、次のif文内のコードを実行
       if (value > 0){
         pleaseSelect.setAttribute("style", "display: none;")
-        // categorySeconds[value - 1]は、選択したカテゴリー1の値に属するカテゴリー2のプルダウン要素
+        // categorySeconds[value - 1]は、カテゴリー1で選択したoption要素に属するカテゴリー2のプルダウン要素
         categorySeconds[value - 1].setAttribute("style", "display: block;")
-        categorySeconds[value - 1].addEventListener("change", (e) => {
-          removeStyle(links)
-          const valueSecond = categorySeconds[value - 1].value
-          // カテゴリー2のプルダウンで「選択してください」以外が選択された場合、次のif文内のコードを実行
-          if (valueSecond > 0){
+
+        // カテゴリー2で選択しているoption要素に対応したリンクを画面上に表示させる関数式
+        const appearLink = () => {
+          // カテゴリー2のプルダウンで「選択してください」以外が選択されている場合、次のif文内のコードを実行
+          if(categorySeconds[value - 1].value > 0){
             // カテゴリー2のoption要素を全て取得
             const categorySecondOptions = document.querySelectorAll(`.cs-index__category-second.num-${value - 1} option`)
-            // selectedされているカテゴリー2のoption要素を変数selectedOptionに代入
-            const selectedOption = categorySecondOptions[valueSecond]
+            // カテゴリー2で選択されているoption要素を変数selectedOptionに代入
+            const selectedOption = categorySecondOptions[categorySeconds[value - 1].value]
             // option要素にdata-id属性としてcategory_secondsテーブルのidの値が割り当てられているので取得
             const dataId = selectedOption.getAttribute("data-id")
             const link = document.querySelector(`.cs-index__link.num-${dataId}`)
             link.setAttribute("style", "display: block;")
           }
+        }
+        
+        appearLink()
+        
+        categorySeconds[value - 1].addEventListener("change", (e) => {
+          removeStyle(links)
+          appearLink()
         })
       }
     })
