ce0cd0133283538ca92643bf4973d5141d7ea6c0
a-washida <59239304+a-washida@users.noreply.github.com>
2020-10-23 23:55:54 +0900

    [追加]画像も複製できるように変更

--- app/controllers/shares_controller.rb
+++ app/controllers/shares_controller.rb
@@ -28,10 +28,12 @@ class SharesController < ApplicationController
   end
 
   def import
-    question_answer_dup = QuestionAnswer.new(dup_params)
+    @question_answer_dup = QuestionAnswer.new(dup_params)
     ActiveRecord::Base.transaction do
-      question_answer_dup.save!
-      RepetitionAlgorithm.create!(interval: 0, easiness_factor: current_user.option.easiness_factor, question_answer_id: question_answer_dup.id)
+      @question_answer_dup.save!
+      attach_dup_image_to_question_option if @question_answer.question_option.image.attached?
+      attach_dup_image_to_answer_option if @question_answer.answer_option.image.attached?
+      RepetitionAlgorithm.create!(interval: 0, easiness_factor: current_user.option.easiness_factor, question_answer_id: @question_answer_dup.id)
     end
     redirect_to back_to_specific_question_position, notice: "問題をグループ[]にインポートしました"
   rescue StandardError => e
@@ -76,4 +78,15 @@ class SharesController < ApplicationController
     }
   end
 
+  def attach_dup_image_to_question_option
+    @question_answer_dup.question_option.image.attach io: StringIO.new(@question_answer.question_option.image.download),
+                                                      filename: @question_answer.question_option.image.filename,
+                                                      content_type: @question_answer.question_option.image.content_type 
+  end
+
+  def attach_dup_image_to_answer_option
+    @question_answer_dup.answer_option.image.attach io: StringIO.new(@question_answer.answer_option.image.download),
+                                                      filename: @question_answer.answer_option.image.filename,
+                                                      content_type: @question_answer.answer_option.image.content_type 
+  end
 end
