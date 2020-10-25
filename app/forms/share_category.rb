40635f04c3b86b76c59f384ce44bcc3eb59ab714
a-washida <59239304+a-washida@users.noreply.github.com>
2020-10-25 20:47:11 +0900

    [追加]フォームオブジェクトshare_category.rbにバリデーションを追加

--- app/forms/share_category.rb
+++ app/forms/share_category.rb
@@ -3,7 +3,8 @@ class ShareCategory
   include ActiveModel::Model
   attr_accessor :category_first_id, :category_second, :question_answer_id
 
-  validates :category_second, presence: true
+  validates :category_first_id, presence: true
+  validates :category_second, presence: true, length: { maximum: 16 }
 
   def save
     category = CategorySecond.where(name: category_second).first_or_initialize(category_first_id: category_first_id)
