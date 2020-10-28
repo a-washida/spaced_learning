5d9527c28f956b197e5cf225bfaa7b65f21014f0
a-washida <59239304+a-washida@users.noreply.github.com>
2020-10-28 22:39:18 +0900

    [追加]Likeモデルにバリデーションを追加

--- app/models/like.rb
+++ app/models/like.rb
@@ -1,4 +1,8 @@
 class Like < ApplicationRecord
+  validates :share_id,
+            presence: true,
+            uniqueness: { case_sensitive: true, scope: :user_id, message: "は1つの問題に1度だけ可能です" }
+
   belongs_to :user
   belongs_to :share
 end
